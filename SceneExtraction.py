41c985398f6cbda00998663507fe294e14e43338
medhini <medhini95@gmail.com>
2015-10-14 19:24:17 +0530

    Added scene extraction

---
+++ SceneExtraction.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+
+sys.path.append('/usr/local/lib/python2.7/site-packages')
+import cv2
+
+cap = cv2.VideoCapture('football.mp4')
+
+while(True):
+
+	ret, frame = cap.read()
+
+	if ret==False: #To make sure no frame after video ends is read
+		break
+
+	cv2.imshow('frame', frame)
+
+	if ret==False or cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
