e71e37b78bd2d1e28871dd81b3330b1b09a03eb5
Ohileshwar Itagi <ohileshwar17@gmail.com>
2015-10-15 19:11:48 +0530

    Extracting multiple scenes from the frames
    Added a counter variable to store all the extracted frames(scenes) in a newly created folder called as "Frames".

--- SceneExtraction.py
+++ SceneExtraction.py
@@ -3,16 +3,17 @@ import sys
 import cv2
 
 cap = cv2.VideoCapture('football.mp4')
-
+counter=0
 while(True):
 
 	ret, frame = cap.read()
 
 	if ret==False: #To make sure no frame after video ends is read
 		break
-
+	
 	cv2.imshow('frame', frame)
-
+	cv2.imwrite("./Frames/Scene_"+`counter`+".jpg",frame)
+	counter=counter+1
 	if ret==False or cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
